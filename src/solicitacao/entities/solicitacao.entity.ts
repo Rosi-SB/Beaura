35b69322dc6ba3a513f9f76f2bd9657d29eb9b97
Rosi <122313206+Rosi-SB@users.noreply.github.com>
2024-10-03 14:50:01 -0300

    Update solicitacao.entity.ts atualizado

--- src/solicitacao/entities/solicitacao.entity.ts
+++ src/solicitacao/entities/solicitacao.entity.ts
@@ -1,4 +1,8 @@
+import { Solicitacao } from "@prisma/client";
+
 export class SolicitacaoEntity implements Solicitacao {
+  cadId: number;
+  proId: number;
   sol_id: number;
   sol_status: string;
   sol_data: Date;
