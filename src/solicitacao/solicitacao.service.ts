d6395f3905f61fb8814eff7cd8dd733191789a21
Rosi <122313206+Rosi-SB@users.noreply.github.com>
2024-10-03 16:56:41 -0300

    Update solicitacao.service.ts atualizado

--- src/solicitacao/solicitacao.service.ts
+++ src/solicitacao/solicitacao.service.ts
@@ -11,19 +11,19 @@ export class SolicitacaoService {
     return this.prisma.solicitacao.create({data :createSolicitacaoDto});
   }
 
-  async findAll() :Promise<SolicitacaoEntity>{
+  async findAll() :Promise<SolicitacaoEntity[]>{
     return this.prisma.solicitacao.findMany();
   }
 
-  async findOne(id: number) :Promise<SolicitacaoEntity>{
-    return this.prisma.solicitacao.findUnique({where:{id}});
+  async findOne(sol_id: number) :Promise<SolicitacaoEntity>{
+    return this.prisma.solicitacao.findUnique({where:{sol_id}});
   }
 
-  async update(id: number, updateSolicitacaoDto: UpdateSolicitacaoDto) :Promise<SolicitacaoEntity>{
-    return this.prisma.solicitacao.update({where:{id}, data :updateSolicitacaoDto});
+  async update(sol_id: number, updateSolicitacaoDto: UpdateSolicitacaoDto) :Promise<SolicitacaoEntity>{
+    return this.prisma.solicitacao.update({where:{sol_id}, data :updateSolicitacaoDto});
   }
 
-  async remove(id: number) :Promise<SolicitacaoEntity>{
-    return this.prisma.solicitacao.delete({where:{id}});
+  async remove(sol_id: number) :Promise<SolicitacaoEntity>{
+    return this.prisma.solicitacao.delete({where:{sol_id}});
   }
 }
