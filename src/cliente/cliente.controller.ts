ddc89c01c608a7d35d6a1b1a4b3e33d924017e65
Rosi <122313206+Rosi-SB@users.noreply.github.com>
2024-10-14 16:37:03 -0300

    Update cliente.controller.ts atualizado

--- src/cliente/cliente.controller.ts
+++ src/cliente/cliente.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { ClienteService } from './cliente.service';
 import { CreateClienteDto } from './dto/create-cliente.dto';
 import { UpdateClienteDto } from './dto/update-cliente.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('cliente')
 export class ClienteController {
@@ -12,21 +13,23 @@ export class ClienteController {
     return this.clienteService.create(createClienteDto);
   }
 
-  @Get()
-  findAll() {
-    return this.clienteService.findAll();
-  }
-
+  // @Get()
+  // findAll() {
+  //   return this.clienteService.findAll();
+  // }
+  @UseGuards(AuthGuard)//protegido
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.clienteService.findOne(+id);
   }
-
+  
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateClienteDto: UpdateClienteDto) {
     return this.clienteService.update(+id, updateClienteDto);
   }
-
+  
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.clienteService.remove(+id);
