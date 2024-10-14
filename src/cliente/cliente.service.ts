50f75c7c2bea5781cef77fe020da6664c4a60b79
Rosi <122313206+Rosi-SB@users.noreply.github.com>
2024-10-14 16:30:51 -0300

    Update cliente.service.ts atualizado

--- src/cliente/cliente.service.ts
+++ src/cliente/cliente.service.ts
@@ -3,18 +3,34 @@ import { CreateClienteDto } from './dto/create-cliente.dto';
 import { UpdateClienteDto } from './dto/update-cliente.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ClienteEntity } from './entities/cliente.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class ClienteService {
   constructor(private readonly prisma : PrismaService){}
   async create(createClienteDto: CreateClienteDto) : Promise<ClienteEntity> {
+    const hash = await bcrypt.hash(createClienteDto.cli_senha, 12);
+    createClienteDto.cli_senha = hash;
     return this.prisma.cliente.create({data:createClienteDto});
   }
-
-  async findAll() : Promise<ClienteEntity[]> {
-    return this.prisma.cliente.findMany();
+  // async findUnique(cli_email: string): Promise<ClienteEntity> {
+  //   if (!cli_email) {
+  //     throw new Error('O campo cli_email não pode estar vazio ou indefinido');
+  //   }
+  
+  //   return this.prisma.cliente.findUnique({
+  //     where: {
+  //       cli_email: cli_email,
+  //     },
+  //   });
+  // }
+  
+  // async findAll() : Promise<ClienteEntity[]> {
+  //   return this.prisma.cliente.findMany();
+  // }
+  async findUnique(cli_email :string) :Promise<ClienteEntity>{
+    return this.prisma.cliente.findUnique({where:{cli_email}});
   }
-
   async findOne(cli_id: number) : Promise<ClienteEntity> {
     return this.prisma.cliente.findUnique({where:{cli_id}});
   }
