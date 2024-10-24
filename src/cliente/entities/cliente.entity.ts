2bf0fef45d19c814674f8635a8895f435b376fb9
Rosi <122313206+Rosi-SB@users.noreply.github.com>
2024-10-24 16:09:40 -0300

    Update cliente.entity.ts atualizado

--- src/cliente/entities/cliente.entity.ts
+++ src/cliente/entities/cliente.entity.ts
@@ -1,6 +1,7 @@
-import { Cliente } from "@prisma/client";
+import { $Enums, Cliente } from "@prisma/client";
 
 export class ClienteEntity implements Cliente {
+    userType: $Enums.UserType;
     cli_id: number;
     cli_email: string;
     cli_senha: string;
